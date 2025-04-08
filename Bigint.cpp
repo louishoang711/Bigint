a61268962f4e5b620e17461dbabc87c6ba9eaa88
louishoang711 <24162056@student.hcmute.edu.vn>
2025-04-08 16:08:01 +0700

    Update and rename CONTEST_001_B1.cpp to Bigint.cpp

--- Bigint.cpp
+++ Bigint.cpp
@@ -165,14 +165,6 @@ struct bigint{
     }
 };
 int main (){
-    long long n ;
-    cin >> n ;
-    bigint a = 0, b = 1, c;
-    for (int i = 2; i <= n; i++) {
-        c = a + b;
-        a = b;
-        b = c;
-    }
-    cout << b;
 
+    return 0 ;
 }
